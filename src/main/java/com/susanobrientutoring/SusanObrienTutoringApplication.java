fb2c9a37e6c0451466e3ee84eb0bd32cae276528
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-08-02 05:42:48 -0400

    init commit

---
+++ src/main/java/com/susanobrientutoring/SusanObrienTutoringApplication.java
@@ -0,0 +1,13 @@
+package com.susanobrientutoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusanObrienTutoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusanObrienTutoringApplication.class, args);
+	}
+
+}
